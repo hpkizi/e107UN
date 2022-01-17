28628a7a8490b766552c02d08d2d8a18adda8b94
Jimmi08 <janamicek@gmail.com>
2022-01-17 15:17:31 +0100

    pm Slovak lans

---
+++ e107_plugins/pm/languages/Slovak_admin.php
@@ -0,0 +1,86 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Slovak Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2019/07/03 14:24:52 
+|            
+|        $URL: https://github.com/e107translations/Slovak/ $
+|        $Revision: 2.2.2 $
+|        $Author: Jimako $
++---------------------------------------------------------------+
+*/
+define("ADLAN_PM_1", "To activate please go to your menus screen and select the private_msg into one of your menu areas. <br /><br />If you need to convert messages from a previous version, please go to the main config page for this plugin and select the 'convert' link.");
+define("ADLAN_PM_3", "Nastavenia SS neboli nájdené, použité prednastavené hodnoty");
+define("ADLAN_PM_4", "Možnosti boli aktualizované");
+define("ADLAN_PM_5", "Obmedzenie pre vybranú triedu užívateľa už existuje");
+define("ADLAN_PM_6", "Obmedzenie bolo úspešne pridané");
+define("ADLAN_PM_7", "Limit nepridaný/neaktualizovaný - neznáma chyba");
+define("ADLAN_PM_8", "Status obmedzenia bol aktualizovaný");
+define("ADLAN_PM_9", " - Obmedzenie bolo úspešne odstránené");
+define("ADLAN_PM_10", " - Obmedzenie nebolo odstránené - neznáma chyba");
+define("ADLAN_PM_11", " - Obmedzenie bolo úspešne aktualizované");
+define("ADLAN_PM_12", "Možnosti SS");
+define("ADLAN_PM_13", "Konverzia SS");
+define("ADLAN_PM_14", "Obmedzenia SS");
+define("ADLAN_PM_15", "Pridať obmedzenie SS");
+define("ADLAN_PM_16", "Názov pluginu");
+define("ADLAN_PM_17", "Zobraziť novú SS animáciu");
+define("ADLAN_PM_18", "Zobraziť užívateľský rozbaľovací zoznam");
+define("ADLAN_PM_19", "Časový limit PREČÍTANEJ správy");
+define("ADLAN_PM_20", "Časový limit NEPREČÍTANEJ správy");
+define("ADLAN_PM_21", "Popup upozornenie na novú SS");
+define("ADLAN_PM_22", "Časový limit pre oneskorenie Popup okna");
+define("ADLAN_PM_23", "Pridať obmedzenie SS");
+define("ADLAN_PM_24", "Počet SS zobrazených na stránku");
+define("ADLAN_PM_25", "Aktivovať emailové upozornenie SS");
+define("ADLAN_PM_26", "Umožniť užívateľovi požadovať prečítanie prijatia emailového upozornenia");
+define("ADLAN_PM_27", "Umožniť zasielanie príloh");
+define("ADLAN_PM_28", "Maximálna veľkosť príloh");
+define("ADLAN_PM_29", "Umožniť zasielanie všetkým členom");
+define("ADLAN_PM_30", "Umožniť zasielanie viacerým príjemcom");
+define("ADLAN_PM_31", "Trieda užívateľov, ktoré môže posielať celým triedam užívateľov");
+define("ADLAN_PM_33", "Neaktívne (bez limitov)");
+define("ADLAN_PM_34", "Súhrny SS");
+define("ADLAN_PM_35", "Rozmery schránky SS");
+define("ADLAN_PM_37", "Výpočet obmedzení");
+define("ADLAN_PM_38", "Veľkosť obmedzení (v KB)");
+define("ADLAN_PM_41", "Momentálne nie sú nastavené žiadne obmedzenia.");
+define("ADLAN_PM_44", "sekundy");
+define("ADLAN_PM_45", "Obmedzenie SS od:");
+define("ADLAN_PM_54", "Hlavné nastavenia");
+define("ADLAN_PM_55", "Obmedzenia");
+define("ADLAN_PM_59", "Údržba");
+define("ADLAN_PM_60", "PM Maintenance");
+define("ADLAN_PM_62", "These options remove transactions concerned with deleted users from the PM database tables");
+define("ADLAN_PM_63", "Správy odoslané");
+define("ADLAN_PM_64", "Správy prijaté");
+define("ADLAN_PM_65", "User blocks");
+define("ADLAN_PM_66", "No maintenance tasks specified");
+define("ADLAN_PM_67", "PM DB maintenance started");
+define("ADLAN_PM_68", "[x] blocks to deleted users removed");
+define("ADLAN_PM_69", "[x] blocks from deleted users removed");
+define("ADLAN_PM_70", "Database error [y]: [z] removing user blocks");
+define("ADLAN_PM_71", "Time-expired messages");
+define("ADLAN_PM_72", "No PM timeouts set");
+define("ADLAN_PM_73", "[x] time-expired PMs deleted");
+define("ADLAN_PM_74", "Removed [x] messages sent by deleted users");
+define("ADLAN_PM_75", "Removed [x] messages received by deleted users");
+define("ADLAN_PM_77", "(Empty all fields and Update to delete a limit)");
+define("ADLAN_PM_78", "Check attachments");
+define("ADLAN_PM_79", "[x] unused attachments deleted. [y] missing attachments noted");
+define("ADLAN_PM_80", "Preference formats updated");
+define("ADLAN_PM_81", "Maximum number of PMs to send immediately");
+define("ADLAN_PM_82", "If sending more than this, they are added to a queue and sent out using the cron task");
+define("ADLAN_PM_83", "Users may only send messages to users in this class");
+define("ADLAN_PM_84", "Maximum message length");
+define("ADLAN_PM_85", "0 = unlimited");
+define("ADLAN_PM_86", "VIP trieda");
+define("ADLAN_PM_87", "Users in this class can only receive messages from others in this class. Overrides the above preference.");
+define("ADLAN_PM_88", "User may send PMs to");
+define("ADLAN_PM_89", "(Akýkoľvek užívateľ s touto triedou)");
+define("ADLAN_PM_90", "Please enter a recipient in the 'To' field.");
+define("ADLAN_PM_91", "Test");
+define("ADLAN_PM_92", "Testovací email odoslaný");
+define("ADLAN_PM_93", "Odoslanie testovacieho emailu zlyhalo!");
---
+++ e107_plugins/pm/languages/Slovak_mailer.php
@@ -0,0 +1,16 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Slovak Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Last Modified: 2017/12/28 13:21:12 
+|            
+|        $URL: https://github.com/e107translations/Slovak/ $
+|        $Revision: 2.1.7 $
+|        $Author: Jimako $
++---------------------------------------------------------------+
+*/
+define("LAN_EC_PM_04", "SS Handler");
+define("LAN_EC_PM_05", "Spracováva veľké množstvo súkromných správ");
+define("LAN_EC_PM_06", "Spustiť spracovanie hromadnej SS pre [y]  príjemcov");
