ff5b0949c8b3e750bc26aec8721a455bcbd1720b
Jimmi08 <janamicek@gmail.com>
2022-01-18 18:03:44 +0100

    Hybridauth update 2.3.2

--- e107_plugins/social/admin_config.php
+++ e107_plugins/social/admin_config.php
@@ -399,12 +399,12 @@ class social_ui extends e_admin_ui
 				'facebook'		=> 	array('label'=>"Facebook", "placeholder"=>"eg. https://www.facebook.com/e107CMS"),
 				'twitter'		=>	array('label'=>"Twitter",	"placeholder"=>"eg. https://twitter.com/e107"),
 				'youtube'		=>	array('label'=>"Youtube",	"placeholder"=>"eg.https://youtube.com/e107Inc"),
-				'linkedin'		=>	array('label'=>"LinkedIn",	"placeholder"=>"eg. http://www.linkedin.com/groups?home=&gid=1782682"),
+				'linkedin'		=>	array('label'=>"LinkedIn",	"placeholder"=>"eg. https://www.linkedin.com/groups?home=&gid=1782682"),
 				'github'		=>	array('label'=>"GitHub",	"placeholder"=>"eg. https://github.com/e107inc"),
 				'flickr'		=>	array('label'=>"Flickr",	"placeholder"=>""),
 				'instagram'		=>	array('label'=>"Instagram",	"placeholder"=>""),
 				'pinterest'		=>	array('label'=>"Pinterest",	"placeholder"=>""),
-				'steam'			=>	array('label'=>"Steam",		"placeholder"=>"eg. http://steamcommunity.com"),
+				'steam'			=>	array('label'=>"Steam",		"placeholder"=>"eg. https://steamcommunity.com"),
 				'vimeo'			=>	array('label'=>"Vimeo",		"placeholder"=>""),
 				'twitch'		=> 	array('label'=>"Twitch", 	"placeholder"=>""),
 				'vk'			=> 	array('label'=>"VK (Vkontakte)", "placeholder"=>""),
@@ -606,6 +606,19 @@ class social_ui extends e_admin_ui
 
 		foreach ($provider_names as $provider_name)
 		{
+			// Check if the current configuration for the provider is valid
+			try
+			{
+				new e_user_provider($provider_name, [], false);
+			}
+			catch (\Hybridauth\Exception\InvalidArgumentException $e)
+			{
+				e107::getMessage()->addError("[{$e->getCode()}] {$e->getMessage()}");
+			}
+			catch (\Hybridauth\Exception\UnexpectedValueException $ignored)
+			{
+			}
+
 			$text .= $this->generateSocialLoginRow($provider_name, $readonly);
 		}
 
@@ -689,6 +702,7 @@ class social_ui extends e_admin_ui
 
 	/**
 	 * @param $provider_name
+	 * @param bool $readonly
 	 * @return string Text to append
 	 */
 	private function generateSocialLoginRow($provider_name, $readonly = false)
