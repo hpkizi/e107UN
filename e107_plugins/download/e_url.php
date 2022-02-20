6f598fcc589901012507413d104059377b079f80
Jimmi08 <janamicek@gmail.com>
2022-02-20 13:24:31 +0100

    attempt to avoid to know only ID just for downloading files

--- e107_plugins/download/e_url.php
+++ e107_plugins/download/e_url.php
@@ -127,7 +127,7 @@ class download_url // plugin-folder + '_url'
 		$config['get']     = array(
 			'regex'		    => '^{alias}/get/([\d]*)/(.*)$',
 			'sef'           => '{alias}/get/{download_id}/{download_sef}',
-			'redirect'	    => '{e_PLUGIN}download/request.php?id=$1', 		// file-path of what to load when the regex returns true.
+			'redirect'	    => '{e_PLUGIN}download/request.php?id=$1&sef=$2', 		// file-path of what to load when the regex returns true.
 		);
 
 		$config['report']    = array(
